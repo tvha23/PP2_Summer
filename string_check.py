73f1f07dffc8dec648ffbcd41d71f20b1f2122f2
tvha23 <ramazan.plt26@gmail.com>
2020-06-09 23:36:30 +0600

    a

---
+++ string_check.py
@@ -0,0 +1,6 @@
+s=input("Enter a string\n")
+s2=input("Enter string to check if it's exists in previous string\n")
+if s2 in s:
+    print("Yes, dude it exists")
+else:
+    print("Bad luck,try again next time?")
